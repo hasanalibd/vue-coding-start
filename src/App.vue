3060cfdd12a32c6f5460495cd2b0941a97fd4dca
hasanalibd <hasanali182838@gmail.com>
2023-08-11 12:20:14 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,12 @@
 <script setup>
-import {ref} from 'vue'
-
-const number = ref(10)
-const animal = ref('')
-const searchUrl = 'https://www.pexels.com/search/'
+    const message = 'Welcome. Get ready to master <em>Vue.js 3!</em>'
+    const date = '<strong>30th June, 2023</strong>'
 </script>
 
 <template>
   <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-      <h1 class="text-4xl">Welcome. Get ready to master Vue.js 3!</h1>
-      <h2 class="mt-10 text-xl text-gray-700">{{ number }} / 4 = {{ number / 4 }}</h2>
-      <h2 class="mt-10 text-xl text-gray-700">{{ number }} is {{ number % 2 == 0 ? "Even" : "Odd"  }}</h2>
-
-      <input type="number" name="" id="" class="border-2 py-2 text-center" placeholder="Check Number" v-model="number">
-
-      <!-- search option -->
-      <!-- <h2 class="mt-10 text-xl text-gray-700">
-        <a target="_blank" :href="searchUrl+animal.toLowerCase()">Search For {{ animal }}</a>
-      </h2> -->
-      
-      <!-- dynamic search -->
-      <h2 class="mt-10 text-xl text-gray-700">
-        <a target="_blank" :href="`https://www.pexels.com/search/${animal.toLowerCase()}`">Search For {{ animal }}</a>
-      </h2>
-
-      <h2 class="mt-10 text-xl text-gray-700">
-        <a target="_blank" :href="`https://www.pexels.com/search/${animal.toLowerCase()}`">Search For {{ animal }}</a>
-      </h2>
-      <input type="text" name="" id="" class="border-2 py-2 text-center" placeholder="search animals" v-model="animal">
+      <h1 class="text-4xl" v-html="message"></h1>
+      <h2 class="mt-10 text-xl text-gray-700">Today is <span v-html="date"></span></h2>
   </div>
 </template>
 
