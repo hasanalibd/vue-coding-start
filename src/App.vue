d0c9d4e257d5564405179bf7482550e7c5ea5003
hasanalibd <hasanali182838@gmail.com>
2023-08-11 10:07:03 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,57 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// export default {
+//   data(){
+//     const message = "Welcome. Get ready to master Vue.js 3 with Ostad"
+//     const date = "10th July, 2023"
+//     return {
+//       message, date
+//     }
+//   },
+//   methods:{
+//     getDate(){
+//       return "11th July"
+//     }
+//   }
+// }
+// export default {
+//   setup(){
+//     const message = "Welcome. Get ready to master Vue.js 3 with Ostad"
+//     const date = "10th July, 2023"
+
+//     function getDate(){
+//       return "11th July, 2024"
+//     }
+
+//     return {
+//       message, date, getDate
+//     }
+//   }
+// }
+
+const message = "Welcome. Get ready to master Vue.js 3 with Ostad"
+const date = "<strong>10th July, 2023</strong>"
+
+
+function getDate() {
+  return "11th July, 2024"
+}
+
+function evenOrOdd(n) {
+  if (n % 2 == 0) {
+    return "Even"
+  }
+  return "Odd"
+}
+const number = 16
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
+    <h1 class="text-4xl">{{ message }}</h1>
+    <!-- <h2 class="mt-10 text-xl text-gray-700">Today is <span> {{ getDate() }}</span></h2> -->
+    <!-- <h2 class="mt-10 text-xl text-gray-700">{{ number }} is {{ evenOrOdd(number) }}</h2> -->
+    <h2 class="mt-10 text-xl text-gray-700">{{ number }} is {{ number % 2 == 0 ? "Even" : "Odd" }}</h2>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
