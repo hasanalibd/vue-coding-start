778939552e9c205f0660c47ebc350057d158c6cb
hasanalibd <hasanali182838@gmail.com>
2023-08-11 10:19:46 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,47 @@
 <script setup>
-// export default {
-//   data(){
-//     const message = "Welcome. Get ready to master Vue.js 3 with Ostad"
-//     const date = "10th July, 2023"
-//     return {
-//       message, date
-//     }
-//   },
-//   methods:{
-//     getDate(){
-//       return "11th July"
-//     }
-//   }
-// }
-// export default {
-//   setup(){
-//     const message = "Welcome. Get ready to master Vue.js 3 with Ostad"
-//     const date = "10th July, 2023"
-
-//     function getDate(){
-//       return "11th July, 2024"
-//     }
-
-//     return {
-//       message, date, getDate
-//     }
-//   }
-// }
-
-const message = "Welcome. Get ready to master Vue.js 3 with Ostad"
-const date = "<strong>10th July, 2023</strong>"
-
-
-function getDate() {
-  return "11th July, 2024"
-}
-
-function evenOrOdd(n) {
-  if (n % 2 == 0) {
-    return "Even"
+const images = [
+  {
+    image: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    title:'Water surrounded by trees and mountains',
+    link:'https://unsplash.com/photos/NRQV-hBF10M'
   }
-  return "Odd"
+]
+// const image = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80'
+// const title = 'Water surrounded by trees and mountains'
+// const link = 'https://unsplash.com/photos/NRQV-hBF10M'
+
+const picture = {
+    src: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    title:'Water surrounded by trees and mountains',
+    alt:'Water surrounded by trees and mountains',
+    link:'https://unsplash.com/photos/NRQV-hBF10M'
 }
-const number = 16
 </script>
 
 <template>
-  <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-    <h1 class="text-4xl">{{ message }}</h1>
-    <!-- <h2 class="mt-10 text-xl text-gray-700">Today is <span> {{ getDate() }}</span></h2> -->
-    <!-- <h2 class="mt-10 text-xl text-gray-700">{{ number }} is {{ evenOrOdd(number) }}</h2> -->
-    <h2 class="mt-10 text-xl text-gray-700">{{ number }} is {{ number % 2 == 0 ? "Even" : "Odd" }}</h2>
-  </div>
+    <section class="container mx-auto p-10">
+        <h1 class="text-4xl mb-10 text-left">Data Project</h1>
+        <div class="grid md:grid-cols-2 gap-5">
+          <section>
+            <div>
+                <img :title="images[0].title" :src="images[0].image" :alt="images[0].title">
+                <h2 class="text-xl text-left py-4"> {{ images[0].title }}</h2>
+                <a target="_blank" :href="images[0].link" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-40 text-center">
+                    Image Source
+                </a>
+            </div>
+          </section>
+          <section >
+            <div>
+                <img v-bind="picture">
+                <h2 class="text-xl text-left py-4"> {{ picture.title }}</h2>
+                <a target="_blank" :href="picture.link" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-40 text-center">
+                    Image Source
+                </a>
+            </div>
+          </section>
+        </div>
+    </section>
 </template>
 
 <style scoped></style>
