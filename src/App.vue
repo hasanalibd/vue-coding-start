c1f62279b5be7515a0f716a8364330f76226a999
hasanalibd <hasanali182838@gmail.com>
2023-08-11 10:38:26 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
-
+const message = 'Welcome. Get ready to master Vue.js 3!'
+const date = '30th June, 2023'
+function getDate(){
+    const date = new Date()
+    return date.toDateString()
+}
 </script>
 
 <template>
-    <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-        <h1 class="text-4xl">Welcome. Get ready to master Vue.js 3!</h1>
-        <h2 class="mt-10 text-xl text-gray-700">Today is <span>30th June, 2023</span></h2>
-    </div>
+  <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
+      <h1 class="text-4xl">{{ message }}</h1>
+      <!-- <h2 class="mt-10 text-xl text-gray-700">Today is <span>{{ date }}</span></h2> -->
+      <h2 class="mt-10 text-xl text-gray-700">Today is <span>{{ getDate() }}</span></h2>
+  </div>
 </template>
 
 <style scoped>
