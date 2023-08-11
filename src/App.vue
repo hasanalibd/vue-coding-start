062f25dece6fc13a136e362c4ccdf143fd6041da
hasanalibd <hasanali182838@gmail.com>
2023-08-11 10:51:41 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <script setup>
-const message = 'Welcome. Get ready to master Vue.js 3!'
-const date = '30th June, 2023'
-
-const data = {
-    message: 'Welcome. Get ready to master Vue.js 3!',
-    date: '30th June, 2023',
-    tasks: [1, 2, 'No', 4, 5, 6]
-}
+// import {data} from './data/data'
+import data from './data/data'
 </script>
 
 <template>
---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+const data = {
+    message: 'Welcome. Get ready to master Vue.js 3!',
+    date: '30th June, 2023',
+    tasks: [1, 2, 'No', 4, 5, 6]
+}
+
+// export {data}
+export default data
